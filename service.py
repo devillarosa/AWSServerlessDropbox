d6f183efb4a5556a64348396c9c9670e0925b0c0
Devin Villarosa <DevinVillarosa92@gmail.com>
2017-08-28 14:16:30 -0700

    Initial Commit

---
+++ service.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+import re
+import collections
+import pymysql
+
+
+def handler(event, context):
+
+    client = boto3.client('s3')
+    bucket_name = 'devinvillarosa-sjsu-public'
+
+    dict_response = client.list_objects(Bucket=bucket_name)
+
+    # extract contents from the original dictionary
+    content_list = dict_response['Contents']
+
+
+    output_dict = {}
+    for content in content_list:
+        content_key = content['Key']
+        url = client.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': content_key})
+        output_dict[content_key] = url
+
+        # Sorts the Dictionary
+        output_dict = collections.OrderedDict(sorted(output_dict.items()))
+
+        # Transforms to JSON notation
+        output = json.dumps(output_dict, ensure_ascii=False)
+        resp = {
+                "statusCode": 200,
+                "headers": {"Access-Control-Allow-Origin": "*"},
+                "body": output
+                }
+
+    return resp
+
+
+
